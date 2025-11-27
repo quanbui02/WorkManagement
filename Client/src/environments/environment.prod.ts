e313cc1d2136cd6639dbaf1a6d0c7f32f30bbf8f
BUI QUAN <bhquaninit27@gmail.com>
2025-11-27 23:17:42 +0700

    f

--- Client/src/environments/environment.prod.ts
+++ Client/src/environments/environment.prod.ts
@@ -14,7 +14,7 @@ export const environment = {
         authorizationEndpoint: 'https://fa.friends.vn/authorization.api',
         fileEndpoint: 'https://faapi.friends.vn/file.api',
         notificationEndpoint: 'https://notification.faapi.friends.vn',
-        workmanagementEndPoint: 'http://wmdev.id.vn',
+        workmanagementEndPoint: 'https://wmdev.id.vn',
         dapEInvoiceEndPoint: 'https://localhost:44319',
         logEndPoint: 'https://logs.mypha.vn',
         documentServer: 'https://documentserver.friends.vn',
